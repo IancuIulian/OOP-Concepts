56ee9f47e4b40dd82d3a24ea7032fff87c1a52f4
Iancu Iulian <iulian.iancu1995@gmail.com>
2017-03-14 15:06:22 +0200

    Add files via upload

---
+++ zAnothers/Polymorphism Concept Challenge/src/PolymorphismConceptChallenge.java
@@ -0,0 +1,40 @@
+/**
+ * Created by ulise on 14/03/2017.
+ */
+public class PolymorphismConceptChallenge {
+
+    public static void main(String[] args) {
+        Person u = new Undergrad();
+        u.method1();
+    }
+}
+
+class Person{
+    public void method1(){
+        System.out.println("Person 1");
+    }
+    public void method2(){
+        System.out.println("Person 2");
+    }
+}
+
+class Student extends Person{
+    @Override
+    public void method1() {
+        System.out.println("Student 1");
+        super.method1();
+        method2();  // here java calls "this.method2();" --> that is Undergrad's method2().
+        // if there was no method2() inside Undergrad class, Student's method2() would have been called
+    }
+    @Override
+    public void method2() {
+        System.out.println("Student 2");
+    }
+}
+
+class Undergrad extends Student{
+    @Override
+    public void method2() {
+        System.out.println("Undergrad 2");
+    }
+}
